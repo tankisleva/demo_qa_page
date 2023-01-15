79ac8a4e9809a8ad16ee42fbe21f047531bdf4cd
Oleg Malyshev <malyshev@jivosite.com>
2023-01-15 12:43:27 +0300

    edit tests and add img

--- framework/model/controls/datepicker.py
+++ framework/model/controls/datepicker.py
@@ -2,5 +2,5 @@ from selene import have
 from selene.support.shared import browser
 
 
-def selectDate(selector, value):
+def select_date(selector, value):
     browser.element(selector).all('option').element_by(have.exact_text(value)).click()
