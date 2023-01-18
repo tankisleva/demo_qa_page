77b46b3b22baf115248a8e476a44763cae4b582f
Oleg Malyshev <malyshev@jivosite.com>
2023-01-18 22:14:21 +0300

    edit conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,9 +46,6 @@ def setup_browser(request):
         options=options
     )
     browser = Browser(Config(driver))
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.base_url = 'https://demoqa.com'
 
     yield browser
 
